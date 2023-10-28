ff549a619f8b2c5634099b2c213c93b39ecfe1a1
tsales18 <128761228+tsales18@users.noreply.github.com>
2023-10-28 23:21:33 +0000

    SMMI.py

--- SMMI.py
+++ SMMI.py
@@ -385,13 +385,7 @@ if fLIDERES == 'IVSON PAULINO':
                                     
                                  
                                     
-echo "# https-github.com-tsales18-SMMI.py" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M SALES
-git remote add origin https://github.com/tsales18/https-github.com-tsales18-SMMI.py.git
-git push -u origin main                          
+                        
                                    
                               
 
