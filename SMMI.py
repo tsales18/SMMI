919f42417293bc2b0512e3e4fea18092b01d8755
tsales18 <128761228+tsales18@users.noreply.github.com>
2023-10-29 21:02:26 -0300

    OK

--- SMMI.py
+++ SMMI.py
@@ -65,7 +65,7 @@ with st.sidebar:
     "mssql+pyodbc",
     username="SALES",
     password="47297913",
-    host="DESKTOP-62QBI08",
+    host="DESKTOP-62QBI08\WINCCPLUSMIG2014",
     port=51304,
     database="SSMM",
     query={
@@ -120,15 +120,15 @@ if st.button('🔄'):
 
           
 
-DATA = pd.read_sql_table("ABERTURA", con = "mssql+pyodbc://DESKTOP-62QBI08\james:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
+DATA = pd.read_sql_table("ABERTURA", con = "mssql+pyodbc://SALES:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
 "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
 "&authentication=ActiveDirectoryIntegrated")
 
-DATA1 = pd.read_sql_table("DADOS", con = "mssql+pyodbc://DESKTOP-62QBI08\james:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
+DATA1 = pd.read_sql_table("DADOS", con = "mssql+pyodbc://SALES:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
 "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
 "&authentication=ActiveDirectoryIntegrated")
 
-DATA2 = pd.read_sql_table("DADOS1", con = "mssql+pyodbc://DESKTOP-62QBI08\james:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
+DATA2 = pd.read_sql_table("DADOS1", con = "mssql+pyodbc://SALES:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
 "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
 "&authentication=ActiveDirectoryIntegrated")
 
@@ -153,13 +153,13 @@ if fLIDERES == 'FELIPE LEITE':
             b0,b1 = st.columns([1,26])
             with b0:
                 if st.button('↻'):
-                    DATA = pd.read_sql_table("ABERTURA", con = "mssql+pyodbc://DESKTOP-62QBI08\james:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
+                    DATA = pd.read_sql_table("ABERTURA", con = "mssql+pyodbc://SALES:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
                     "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
                     "&authentication=ActiveDirectoryIntegrated")
-                    DATA1 = pd.read_sql_table("DADOS", con = "mssql+pyodbc://DESKTOP-62QBI08\james:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
+                    DATA1 = pd.read_sql_table("DADOS", con = "mssql+pyodbc://SALES:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
                     "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
                     "&authentication=ActiveDirectoryIntegrated")
-                    DATA2 = pd.read_sql_table("DADOS1", con = "mssql+pyodbc://DESKTOP-62QBI08\james:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
+                    DATA2 = pd.read_sql_table("DADOS1", con = "mssql+pyodbc://SALES:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
                     "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
                     "&authentication=ActiveDirectoryIntegrated")
             st.markdown("---")
@@ -278,11 +278,11 @@ if fLIDERES == 'FELIPE LEITE':
                     st.dataframe(df6)
                     
                     
-DATA3 = pd.read_sql_table("ABERTURAF", con = "mssql+pyodbc://DESKTOP-62QBI08\james:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
+DATA3 = pd.read_sql_table("ABERTURAF", con = "mssql+pyodbc://SALES:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
 "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
 "&authentication=ActiveDirectoryIntegrated")
 
-DATA4 = pd.read_sql_table("DADOSF", con = "mssql+pyodbc://DESKTOP-62QBI08\james:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
+DATA4 = pd.read_sql_table("DADOSF", con = "mssql+pyodbc://SALES:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
 "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
 "&authentication=ActiveDirectoryIntegrated")
 
@@ -303,10 +303,10 @@ if fLIDERES == 'IVSON PAULINO':
             b3,b4 = st.columns([1,26])
             with b3:
                 if st.button('↻'):
-                    DATA3 = pd.read_sql_table("ABERTURAF", con = "mssql+pyodbc://DESKTOP-62QBI08\james:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
+                    DATA3 = pd.read_sql_table("ABERTURAF", con = "mssql+pyodbc://SALES:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
                     "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
                     "&authentication=ActiveDirectoryIntegrated")
-                    DATA4 = pd.read_sql_table("DADOSF", con = "mssql+pyodbc://DESKTOP-62QBI08\james:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
+                    DATA4 = pd.read_sql_table("DADOSF", con = "mssql+pyodbc://SALES:47297913@DESKTOP-62QBI08\WINCCPLUSMIG2014:51304/SSMM?"
                     "driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
                     "&authentication=ActiveDirectoryIntegrated")
             st.markdown("---")
