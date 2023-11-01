94dc873e26bcf0ae346b4a8c4ca0a4c324dfe052
tsales18 <128761228+tsales18@users.noreply.github.com>
2023-11-01 22:47:08 +0000

    ok

--- SMMI.py
+++ SMMI.py
@@ -118,38 +118,25 @@ if fLIDERES == 'FELIPE LEITE':
                         st.markdown("---")
                         atd = st.toggle('Atualizar os dados')
                         solicitante = st.selectbox('Solicitante', ('FILIPE','JAMESON','MAURILIO SALES','BRUNO KAPPAUN','EDUARDO BICUDO','ADRIELY LEMOS','GILSON FREITAS','ALEX SANTOS','CESAR AUGUSTO'),index=None,placeholder='Selecione')
-                        if atd:
-                            Usolicitante = st.selectbox('Atualize o Solicitante', ('FILIPE','JAMESON','MAURILIO SALES','BRUNO KAPPAUN','EDUARDO BICUDO','ADRIELY LEMOS','GILSON FREITAS','ALEX SANTOS','CESAR AUGUSTO'),index=None,placeholder='Selecione')
-                            st.markdown("---")
+                        
 
                         status = st.selectbox('Tipo de Ocorrência', ('ELETRICA PREDIAL MANUTENÇÃO EM PAINES TROCA DE COMPONENTES',),index=None, placeholder='Selecione')
-                        if atd:
-                            Ustatus = st.selectbox('Atualize o Tipo de Ocorrência', ('ELETRICA PREDIAL MANUTENÇÃO EM PAINES TROCA DE COMPONENTES'),index=None, placeholder='Selecione')
-                            st.markdown("---")
+                        
 
                         setor = st.selectbox('Setor', ('TECNOLOGIA DA INFORMAÇÃO','ELETRICA'),index=None,placeholder='Selecione')
-                        if atd:
-                            Usetor = st.selectbox('Aualize o Setor', ('TECNOLOGIA DA INFORMAÇÃO','ELETRICA'),index=None,placeholder='Selecione')
-                            st.markdown("---")
+                       
 
                         niveldaocorrencia = st.selectbox('Nivel da ocorrência', ('EMERGÊNCIA','MUITO URGÊNTE','POUCO URGÊNTE','URGÊNTE'),index=None,placeholder='Selecione')
-                        if atd:
-                            Univeldaocorrencia = st.selectbox('Atualize o Nivel da ocorrência',('EMERGÊNCIA','MUITO URGÊNTE','POUCO URGÊNTE','URGÊNTE'),index=None, placeholder='Selecione')
-                            st.markdown("---")
+                        
 
                         relatorio = st.text_input('Relatorio')
-                        if atd:
-                            Urelatorio = st.text_input('Atualize o Relatorio')
-                            st.markdown("---")
+                       
 
                         tempoi = st.time_input('Horario', value=None)
-                        if atd:
-                            Utempoi = st.time_input('Atualize o Horario', value=None)
-                            st.write(tempoi)
+                     
 
                         data = st.date_input("Data", value=None)
-                        if atd:
-                           Udata = st.date_input("Atualize a Data", value=None)
+                       
                         st.form_submit_button('↻')
                 with neymar:
                     with st.form('my form3'):
