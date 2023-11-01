40e397e5186bdd91840c3788887c6c510e2b567a
tsales18 <128761228+tsales18@users.noreply.github.com>
2023-11-01 17:52:22 +0000

    ok

--- SMMI.py
+++ SMMI.py
@@ -136,9 +136,9 @@ if fLIDERES == 'FELIPE LEITE':
                                             st.caption('É necessario finalizar esta OS antes de inciar outra.')
                                             att = st.button("INSERIR DADOS")
                                             if att:
-                                               st.session_state.OS += 1
+                                               cnt1 = cnt1 + 1
                                             if att:
-                                               st.session_state.OS += 1
+                                               cnt1 = cnt1 + 1
                                                st.balloons()
                                                cursor.execute("INSERT INTO ABERTURA (OS,SOLCITANTE,SETOR,TIPO_DE_OCORRENCIA,NIVEL_DA_OCORRENCIA,DATA) VALUES (?, ?, ?, ?, ?,?)", (st.session_state.OS, str(solicitante), str(setor), str(status),str(niveldaocorrencia),data))
                                                conn.commit()
