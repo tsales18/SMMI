c7a530f183b3144a704fa2871817271159777b9d
tsales18 <128761228+tsales18@users.noreply.github.com>
2023-11-01 21:35:19 +0000

    ok

--- SMMI.py
+++ SMMI.py
@@ -119,7 +119,7 @@ if fLIDERES == 'FELIPE LEITE':
                         atd = st.toggle('Atualizar os dados')
                         solicitante = st.selectbox('Solicitante', ('Selecione','FILIPE',),index=0)
                         if atd:
-                            solicitante1 = st.selectbox('Solicitante', ('Selecione','FILIPE','jameson'))
+                            solicitante1 = st.selectbox('Solicitante', ('Selecione','FILIPE',))
                         status = st.selectbox('Tipo de Ocorrência', ('Selecione','ELETRICA PREDIAL MANUTENÇÃO EM PAINES TROCA DE COMPONENTES'),index=0,)  
                         st.markdown("---")
                         setor = st.selectbox('Setor', ('Selecione','TECNOLOGIA DA INFORMAÇÃO',), index=0)
