f38e9efd0096955ba5d38caa7edabcd71f548b85
tsales18 <128761228+tsales18@users.noreply.github.com>
2023-11-02 12:19:28 +0000

    ok

--- SMMI.py
+++ SMMI.py
@@ -150,8 +150,9 @@ if fLIDERES == 'FELIPE LEITE':
                         data = st.date_input("Data", value=None)
                         if atd:
                             Udata = st.date_input("Atualize a Data", value=None)
-                            st.button('atualize')
                         st.form_submit_button('↻')
+                        if atd:
+                            st.button('atualize')
                     
                         
                 if fLIDERES == 'FELIPE LEITE':
