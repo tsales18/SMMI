cacb65cc6932b729a03a7ca6d80e3de547c2c747
tsales18 <128761228+tsales18@users.noreply.github.com>
2023-11-05 02:23:20 +0000

    OK

--- SMMI.py
+++ SMMI.py
@@ -526,9 +526,8 @@ if fLIDERES == 'ROSIVALDO':
                             if setorescolhido == 'PRODUÇÃO':    
                                 fnl=st.button("FINALIZAR")
                                 if fnl:
-                                    cursor1.execute("UPDATE PRODUCAO SET FINALIZADA = ?, DATAF = ?, HORAF = ? WHERE OS = ?",(finalizar,datainput,str(timeinput),fnlz2))
-                                    conn1.commit()
-                                    conn1.close()
+                                    cursor5.execute("UPDATE PRODUCAO SET FINALIZADA = ?, DATAF = ?, HORAF = ? WHERE OS = ?",(finalizar,datainput,str(timeinput),fnlz2))
+                                    conn5.commit()
                                     st.caption('Dia muito lindo é mais que o infinito é puro e belo inocente como uma flor.')   
 
             with tab8:
