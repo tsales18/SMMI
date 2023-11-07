f05862a890a13213ea37af773dcd91cd0c5f9c4d
tsales18 <128761228+tsales18@users.noreply.github.com>
2023-11-07 16:01:05 -0300

    Add files via upload

--- SMMI.py
+++ SMMI.py
@@ -1000,7 +1000,7 @@ if fLIDERES == 'EQUIPE DE ELÉTRICA':
                             osespec = rd65.loc[numros5]
                             def load_data():
                                 return pd.DataFrame(osespec)
-                            st.checkbox("Estender", value=True, key="use_containeEr_width13")
+                            st.checkbox("Estender", value=True, key="use_container_width13")
                             lddt = load_data()
                             st.dataframe(lddt, use_container_width=st.session_state.use_container_width13)
             
