6a5ea042e1ad0efdbf5c2d4d9cc6249b619b8fd9
tsales18 <128761228+tsales18@users.noreply.github.com>
2024-01-23 09:43:22 -0300

    Update P.C.M.py

--- pages/P.C.M.py
+++ pages/P.C.M.py
@@ -103,12 +103,19 @@ datenow = datetime.now().date()
 monthnow = datetime.now().strftime('%B')
 monthnumbernow = datetime.now().month
 
+from datetime import datetime
+import pytz
+
+# Defina o fuso horário desejado
 novo_fuso_horario = pytz.timezone('America/Sao_Paulo')
 
-hora_atual = datetime.datetime.now()
+# Obtenha a hora atual com o fuso horário padrão
+hora_atual = datetime.now()
 
+# Converta a hora atual para o novo fuso horário
 hora_atual_no_novo_fuso = hora_atual.astimezone(novo_fuso_horario)
 
+# Agora, hora_atual_no_novo_fuso contém a hora local em 'America/Sao_Paulo'
 hora_atual_no_novo_fuso
 
 senha = ()
